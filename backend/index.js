8e2026bc01cf5260a86a3876eca8cb83ef4bc069
RobertOneFive <roberthola15@gmail.com>
2023-01-31 12:45:30 +0545

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const connectDB = async ()=>{
+mongoose.set('strictQuery', true);
+
+mongoose.connect('mongodb://localhost:27017/eCommerce');
+    const productSchema = new mongoose.Schema({});
+    const Product = mongoose.model('Product', productSchema);
+    const data = await Product.find({});
+    console.warn(data);
+}
+connectDB();
+app.listen(5000)
