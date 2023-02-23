5962493b5d66728bb841b0cec536b1909192efa8
RobertOneFive <roberthola15@gmail.com>
2023-02-23 21:20:23 +0545

    some changes

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const connectDB = async ()=>{
-mongoose.set('strictQuery', true);
 
-mongoose.connect('mongodb://localhost:8000/eCommerce');
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/eCommerce', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      socketTimeoutMS: 120000
+    });
+
     const productSchema = new mongoose.Schema({});
     const Product = mongoose.model('Product', productSchema);
     const data = await Product.find({});
-    console.warn(data);
-}
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 connectDB();
-app.listen(5000)
+
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
+});
