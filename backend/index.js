9b24a8f8ccca7775d041bdfdf9a30a82bb2f1624
RobertOneFive <roberthola15@gmail.com>
2023-05-02 16:43:25 +0545

    Upto Deletion of Product

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require('./db/config');
 const User = require("./db/User");
+const Product = require("./db/Product");
 const app = express();
 
 app.use(express.json());
@@ -31,6 +32,40 @@ app.post("/login", async (req,resp) => {
     resp.send({result: 'No User Found'})
   }
 })
+
+app.post("/add-product", async (req, resp)=>{
+  let product = new Product(req.body);
+  let result = await product.save();
+  console.warn(req.body);
+  resp.send(result);
+
+})
+// app.get("/add-product", async (req, resp)=>{
+//   let result = await Product.find();
+//   console.warn(result);
+//   resp.send(result);
+// })
+
+app.get("/products", async (req,resp)=>{
+  let products = await Product.find();
+  if(products.length>0)
+  {
+    resp.send(products);
+  }
+  else
+  {
+    resp.send({result:"No Products found"});
+  }
+
+})
+
+app.delete("/product/:id", async (req, resp)=> {
+  const result = await Product.deleteOne({_id:req.params.id})
+  resp.send(result);
+
+})
+
+
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000');
--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -11,22 +11,20 @@ const Nav=()=>{
 
     return(
         <div>
-            <ul className="nav-ul">
+            <img src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" alt="logo" className="logo" />
+            { auth ?<ul className="nav-ul">
                 <li><Link to="/">Products</Link></li>
                 <li><Link to="/add">Add Product</Link></li>
                 <li><Link to="/update">Update Product</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-               {
-                auth ? <li><Link onClick={logout} to='/signup'>Logout</Link>
-                    </li>
-                    :<>
+                <li><Link onClick={logout} to='/signup'>Logout ({JSON.parse(auth).name})</Link></li>
+                </ul>
+                :
+                <ul className="nav-ul nav-right">
                     <li><Link to ="/signup">Sign Up</Link></li>
                     <li><Link to ="/login">Login</Link></li>
-
-                    </>
-               }
-
-            </ul>
+                </ul>
+               }   
         </div>
     )
 }
