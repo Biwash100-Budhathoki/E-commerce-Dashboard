a411712d9203947e34f175ee5ec73b462f381d45
RobertOneFive <roberthola15@gmail.com>
2023-05-03 00:14:01 +0545

    Upto Product Update

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
 const ProductList =()=>{
 
@@ -46,7 +47,10 @@ const ProductList =()=>{
                    <li>{item.name}</li>
                    <li>${item.price}</li>
                    <li>{item.category}</li>
-                   <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
+                   <li><button onClick={()=>deleteProduct(item._id)}>Delete</button>
+                   <Link to={"/update/"+item._id}>Update</Link>
+                   
+                   </li>
                </ul>
            )
                 
