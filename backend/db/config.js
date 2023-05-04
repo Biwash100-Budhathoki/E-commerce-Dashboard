654b83d8a99ce58445f68d32a89fc6e5a01bec16
RobertOneFive <roberthola15@gmail.com>
2023-05-04 18:37:14 +0545

    Revert "upto jwt authentication"
    This reverts commit 3d4eb578d29f8fd08a8a2b28be072319f316480f.

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,3 +1,2 @@
 const mongoose = require('mongoose');
 mongoose.connect("mongodb://127.0.0.1:27017/e-commerce");
-//mongoose.connect("mongodb+srv://biwash10:biwash@1233@cluster0.mongodb.net/e-commerce?retryWrites=true&w=majority");
